e6518aee100b433e02b411d09d1f9f173bd3ecbd
Arthur Evangelista Oliveira <arturevangelistaoliveira@gmail.com>
2025-04-17 14:59:00 -0300

    finalizado algumas animações

--- LandingPage/index.js
+++ LandingPage/index.js
@@ -1,15 +1,17 @@
 const iconBG = document.getElementById('iconBG')
 const header = document.querySelector('header')
 const main = document.querySelector('main')
+const a = document.querySelector('a')
 
 iconBG.addEventListener('click' , function(){
     
     if(document.body.style.backgroundColor === "black"){
-        
+
         document.body.style.backgroundColor = "white"
         document.body.style.color = "#000"
 
         header.style.backgroundColor = "#cad3db"
+        a.style.color = "#000"
 
         iconBG.innerHTML = '<img src="icons/luaPreta.png" alt="icon da lua" >  '
 
@@ -19,6 +21,9 @@ iconBG.addEventListener('click' , function(){
         document.body.style.color = "#fff"
         
         header.style.backgroundColor = "#333"
+
+        a.style.color = "#fff"
+        a.style.textDecoration = "none"
 
 
         iconBG.innerHTML = '<img src="icons/solPreto.png" alt="icon do sol">'
