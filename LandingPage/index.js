9cc040fb473ad25ba9277fc26484d6328bcee482
Arthur Evangelista Oliveira <arturevangelistaoliveira@gmail.com>
2025-04-17 23:53:12 -0300

    Mudei o código da parte do JS para se misturar com o CSS

--- LandingPage/index.js
+++ LandingPage/index.js
@@ -1,33 +1,10 @@
 const iconBG = document.getElementById('iconBG')
-const header = document.querySelector('header')
-const main = document.querySelector('main')
-const a = document.querySelector('a')
+const body = document.body
 
-iconBG.addEventListener('click' , function(){
-    
-    if(document.body.style.backgroundColor === "black"){
+iconBG.addEventListener('click', function () {
+    body.classList.toggle('dark-mode') // Alterna a classe "dark-mode", meio que ele liga e desliga esse dark-mode
 
-        document.body.style.backgroundColor = "white"
-        document.body.style.color = "#000"
+    const isDark = body.classList.contains('dark-mode') //Aqui eu apenas estou conferindo se o body tem essa class "dark-mode"
 
-        header.style.backgroundColor = "#cad3db"
-        a.style.color = "#000"
-
-        iconBG.innerHTML = '<img src="icons/luaPreta.png" alt="icon da lua" >  '
-
-    } else{
-        
-        document.body.style.backgroundColor = "black"
-        document.body.style.color = "#fff"
-        
-        header.style.backgroundColor = "#333"
-
-        a.style.color = "#fff"
-        a.style.textDecoration = "none"
-
-
-        iconBG.innerHTML = '<img src="icons/solPreto.png" alt="icon do sol">'
-        
-        
-    }
+    iconBG.innerHTML = isDark ? '<img src="icons/solPreto.png" alt="icon do sol">': '<img src="icons/luaPreta.png" alt="icon da lua">'
 })
